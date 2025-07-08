d5cb2916f8a2698687ffb77121361dc2f2d0a589
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-07-07 20:59:50 -0600

    adding new files

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { NavbarSimple } from "../components/NavBarMUI2"
+import Stack from "../components/StackIcons"
 
 export default function Page() {
     return(
@@ -22,7 +23,7 @@ export default function Page() {
                     @xs:@min-sm:mt-14
                     
                     @md:@min-lg:size-80
-                    @md:@min-lg:ml-23
+                    @md:@min-lg:ml-20
                     @md:@min-lg:mt-13
 
                     @xl:@min-2xl:size-90
@@ -37,10 +38,13 @@ export default function Page() {
                     src="https://github.com/Gusttav28.png"  
                     alt="" />
                 </div>
-                <div className="">
+                <div className="@xs:@min-sm:items-center @md:@min-lg:ml-20">
                     <h1 className="
                     font-bold
                     text-base 
+                    @xs:@min-sm:text-2xl @xs:@min-sm:ml-15 @xs:@min-sm:mt-10
+                    @md:@min-lg:-ml-7 @md:@min-lg:mt-50 @md:@min-lg:text-3xl
+
                     @5xl:@min-6xl:text-4xl
                     @5xl:@min-6xl:mt-50
                     @5xl:@min-6xl:mr-50
@@ -48,8 +52,37 @@ export default function Page() {
                     
                     ">Hey! Hi you there, My Name is Gustavo! And I'm Software Engineer.</h1>
                 </div>
+                <div className="
+                    @3xs:@min-2xs:mt-4 @3xs:@min-2xs:-ml-130
+                    @xs:@min-sm:-mt-15 @xs:@min-sm:-ml-130
+                    @md:@min-lg:mt-28
+                    @md:@min-lg:-ml-120
+                    @5xl:@min-6xl:mt-30 @5xl:@min-6xl:-ml-80
+                ">
+                    <Stack/>
+                </div>
+                <div className="
+                    @xs:@min-sm:p-3 @xs:@min-sm:-mt-5
+                    @md:@min-lg:mt-10 @md:@min-lg:-ml-90 @md:@min-lg:mr-10
+                    @xl:@min-2xl:-ml-90 @xl:@min-2xl:p-1 @xl:@min-2xl:mr-10
+                    @5xl:@min-6xl:-ml-104 @5xl:@min-6xl:max-w-fit
+                ">
+                    <p className="
+                    @xs:@min-sm:text-xl
+                    @md:@min-lg:text-2xl @md:@min-lg:-ml-10
+                    @xl:@min-2xl:text-2xl
+                    ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque commodi reprehenderit quos quaerat esse facere culpa eos necessitatibus? A, molestiae? Fugit vel sit iste consequuntur blanditiis laborum eaque, molestiae dolor? Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quis magnam eum aperiam optio consectetur adipisci laboriosam sequi fugiat illum sit natus consequuntur, voluptas quaerat explicabo animi veritatis magni nam possimus alias inventore non! Tempore voluptatem vel totam modi saepe voluptas officia assumenda
+                    </p>
+                    <p className="
+                    @xs:@min-sm:text-xl @xs:@min-sm:mt-10
+                    @md:@min-lg:text-2xl @md:@min-lg:-ml-10 @md:@min-lg:mt-10
+                    @xl:@min-2xl:text-2xl @xl:@min-2xl:mt-10">
+                    illo, iusto, molestiae mollitia laboriosam repellat! Hic alias voluptates sunt maxime est, tenetur error ea suscipit doloremque delectus autem recusandae, blanditiis repellat, possimus culpa magnam commodi veniam molestias optio ad aspernatur nemo. Incidunt id cumque fugiat debitis nihil hic, nobis perferendis eveniet deleniti? Iusto ducimus laboriosam quibusdam possimus exercitationem sapiente nihil quasi tempore, tenetur dolore excepturi placeat.</p>
+                </div>
+                <section>
+                    <h1>My current years here</h1>
+                </section>
             </section>
-
         </div>
     )
 }
